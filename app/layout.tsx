e4fc486268ec6ad52eb5ec7f27b44ba84e877553
Shashank Sharma <79395827+valyrian24052@users.noreply.github.com>
2025-06-26 08:26:02 +0000

    aaaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import { Toolbar } from "basehub/next-toolbar";
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import Footer from "./components/footer";
-import { CMS_NAME } from "@/lib/constants";
 import "./globals.css";
 
 export const metadata: Metadata = {
