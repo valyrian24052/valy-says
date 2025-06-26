8043f9c315f36a2d43c35186ece5c91ec76ab135
Shashank Sharma <79395827+valyrian24052@users.noreply.github.com>
2025-06-26 08:28:33 +0000

    aa

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -6,27 +6,6 @@ export function Intro() {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Valy says.
       </h1>
-      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline hover:text-orange-500 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and{" "}
-        <a
-          href={CMS_URL}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline hover:text-orange-500 duration-200 transition-colors"
-        >
-          {CMS_NAME}
-        </a>
-        .
-      </h2>
     </section>
   );
 }
